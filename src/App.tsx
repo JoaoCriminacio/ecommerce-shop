c7f363fb07bb5129ee4ce0d2ddad889c9f205229
João Ricardo Criminácio <joaoricardocriminacio@gmail.com>
2025-11-03 19:06:22 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Routes } from "react-router-dom"
+
+function App() {
+
+  return (
+    <div className="wrapper">
+
+      <main>
+        <Routes>
+          
+        </Routes>
+      </main>
+      
+    </div>
+  )
+}
+
+export default App
